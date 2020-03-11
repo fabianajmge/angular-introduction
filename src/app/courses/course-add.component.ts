89871e9d844b3a9b30fd9ccf674d9c36f20fae0f
fabiana.leao <fabiana.leao@gpnet.com.br>
2020-03-11 14:26:26 -0300

    Validação de campos obrigatorios

--- src/app/courses/course-add.component.ts
+++ src/app/courses/course-add.component.ts
@@ -1,21 +1,25 @@
+import { CourseService } from './course.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 
 @Component({
-  templateUrl: './course-add.component.html'
+  templateUrl: './course-add.component.html',
+  styleUrls: ['./course-add.component.css']
 })
 export class CourseAddComponent {
 
-  constructor(private route: Router) {}
+  constructor(private route: Router, private courseService: CourseService) {}
 
   course = new Course();
 
   save(form: NgForm): void {
-    console.log('form', form)
-    console.log('course', this.course)
-  }
+    this.courseService.save(this.course).subscribe({
+      next: course => console.log('Saved with sucess', course),
+      error: err => console.log('Error', err)
+  });
+}
 
   back(): void {
     this.route.navigate(['/courses']);
