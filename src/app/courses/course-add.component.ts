945d9fa34d37ff47135a4c781bf8885923dafc17
fabiana.leao <fabiana.leao@gpnet.com.br>
2020-03-11 12:03:25 -0300

    Começando a implementar o cadastro

--- src/app/courses/course-add.component.ts
+++ src/app/courses/course-add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   templateUrl: './course-add.component.html'
@@ -9,9 +10,12 @@ export class CourseAddComponent {
 
   constructor(private route: Router) {}
 
-  course: Course;
+  course = new Course();
 
-  save(): void {}
+  save(form: NgForm): void {
+    console.log('form', form)
+    console.log('course', this.course)
+  }
 
   back(): void {
     this.route.navigate(['/courses']);
